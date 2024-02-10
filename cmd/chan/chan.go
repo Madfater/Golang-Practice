5fb49de250bf02227ca57543d7ccb5e1e3beae8a
Madfater <ericleeddd@gmail.com>
2024-02-10 19:28:14 +0800

    feat: 協程練習

--- cmd/chan/chan.go
+++ cmd/chan/chan.go
@@ -5,33 +5,28 @@ import (
 	"time"
 )
 
-// 定義一個函數，該函數作為一個協程運行
-func printNumbers() {
-	for i := 1; i <= 5; i++ {
-		fmt.Println(i)
-		time.Sleep(time.Second) // 模擬耗時操作
-	}
+func producer(ch chan string) {
+	fmt.Println("producer start")
+	ch <- "a"
+	ch <- "b"
+	ch <- "c"
+	ch <- "d"
+	fmt.Println("producer end")
 }
 
-func printStrings() {
-	var arr = [5]string{"a", "b", "c", "d", "e"}
-	for _, v := range arr {
-		fmt.Println(v)
-		time.Sleep(2*time.Second)
+func customer(ch chan string) {
+	for {
+		msg := <-ch
+		fmt.Println(msg)
 	}
 }
 
 func main() {
-	// 啟動一個新的協程，並在其中運行 printNumbers 函數
-	go printNumbers()
-	go printStrings()
+	fmt.Println("main start")
+	ch := make(chan string, 1)
+	go producer(ch)
+	go customer(ch)
 
-	// 主協程繼續執行
-	fmt.Println("Main goroutine continues...")
-
-	// 主協程等待一段時間，以確保 printNumbers 協程有足夠的時間運行
-	time.Sleep(2 * time.Second)
-
-	// 主協程完成
-	fmt.Println("Main goroutine exits.")
+	time.Sleep(1 * time.Second)
+	fmt.Println("main end")
 }
